66a7cda7de0744405cef34d87fed1b742611ced4
Danylo Halaiko <dan.galaiko@gmail.com>
2020-05-10 19:01:40 +0300

    SpainCountryFromFile has been written.

--- src/main/java/com/d9nich/com/Main.java
+++ src/main/java/com/d9nich/com/Main.java
@@ -5,6 +5,7 @@ import com.google.maps.GeoApiContext;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.DistanceMatrix;
 
+import java.io.File;
 import java.io.IOException;
 
 
@@ -12,6 +13,9 @@ public class Main {
     private static final GeoApiContext geoApiContext = new GeoApiContext.Builder().apiKey("your_key").build();
 
     public static void main(String[] args) throws InterruptedException, ApiException, IOException {
+        String[] cities = SpainCountryFromFile.getCities(new File("src/main/java/com/d9nich/com/es.json"));
+        int[][] roadMatrix = SpainCountryFromFile.getRoads(
+                new File("src/main/java/com/d9nich/com/spainCountriesConection.txt"));
         String[] origins =
                 new String[]{
                         "Perth, Australia", "Sydney, Australia", "Melbourne, Australia",
---
+++ src/main/java/com/d9nich/com/SpainCountryFromFile.java
@@ -0,0 +1,60 @@
+package com.d9nich.com;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SpainCountryFromFile {
+    private SpainCountryFromFile() {
+    }
+
+    public static String[] getCities(File file) {
+        return getCities(file, 15);
+    }
+
+    public static String[] getCities(File file, int numberOfCountriesToRead) {
+        Gson gson = new GsonBuilder().create();
+        StringBuilder stringBuilder = new StringBuilder();
+        try (Scanner input = new Scanner(new BufferedInputStream(new FileInputStream(file)))) {
+            while (input.hasNext()) {
+                stringBuilder.append(input.nextLine());
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+        final Country[] countries = gson.fromJson(stringBuilder.toString(), Country[].class);
+        return citiesName(countries, numberOfCountriesToRead);
+    }
+
+    private static String[] citiesName(Country[] countries, int numberOfFirst) {
+        String[] citiesName = new String[numberOfFirst];
+        for (int i = 0; i < numberOfFirst; i++)
+            citiesName[i] = countries[i].getCity();
+        return citiesName;
+    }
+
+    public static int[][] getRoads(File file) {
+        try (Scanner input = new Scanner(new BufferedInputStream(new FileInputStream(file)))) {
+            String[] lines = input.nextLine().split("\\s");
+            int[][] matrixOfRoad = new int[lines.length][lines.length];
+            for (int i = 0; i < lines.length; i++)
+                matrixOfRoad[0][i] = Integer.parseInt(lines[i]);
+            int j = 1;
+            while (input.hasNext()) {
+                lines = input.nextLine().split("\\s");
+                for (int i = 0; i < lines.length; i++)
+                    matrixOfRoad[j][i] = Integer.parseInt(lines[i]);
+                j++;
+            }
+            return matrixOfRoad;
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+        return null;
+    }
+}
